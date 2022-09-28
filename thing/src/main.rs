f1550d8959033495339f8d02e8fba323d9e9954a
Sanity-to-zero <blazingtorch.spam@gmail.com>
2022-09-28 15:01:23 -0700

    modified:   thing/src/main.rs

--- thing/src/main.rs
+++ thing/src/main.rs
@@ -7,6 +7,23 @@
 //each loop check if any countdown is True
 //if all empty, add to list of primes and key/value
 
+use std::io::Stdin;
+struct Prime (u32, u32);
 fn main() {
+    let limit:u32 = Stdin::read_line(&self, buf);
     println!("Hello, world!");
+    let primesList: std::vec::Vec<Prime>;
+    let mut count: u32 = 1;
+    while count < limit{
+        let mut primeTrue:bool = true;
+        for p in primesList{
+            if count % p.1 == 0 {
+                primeTrue = false;
+            }
+        }
+        if !primeTrue {
+            primesList.push((count, count))
+        }
+    }
+
 }
