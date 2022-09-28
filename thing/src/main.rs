479ef1ea25886b6e255dac3f21cbb91965c386e0
Pufferunpopped <blazingtorch.spam@gmail.com>
2022-09-27 22:06:59 -0700

    modified:   thing/src/main.rs

--- thing/src/main.rs
+++ thing/src/main.rs
@@ -1,3 +1,12 @@
+// hopefully optimized Erastothenes sieve
+//structure: take input of limit
+//start incremental loop
+//every prime is added to a key,value list
+//key is the prime 
+//value is countdown before next multiple
+//each loop check if any countdown is True
+//if all empty, add to list of primes and key/value
+
 fn main() {
     println!("Hello, world!");
 }
