f17f958506274033868865a1027f6d9c50da527b
Sanity-to-zero <blazingtorch.spam@gmail.com>
2022-10-03 09:48:16 -0700

    modified:   thing/src/main.rs     fixed code errors

--- thing/src/main.rs
+++ thing/src/main.rs
@@ -7,23 +7,30 @@
 //each loop check if any countdown is True
 //if all empty, add to list of primes and key/value
 
-use std::io::Stdin;
+use std::io;
 struct Prime (u32, u32);
 fn main() {
-    let limit:u32 = Stdin::read_line(&self, buf);
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("failed to readline");
+    let limit:u32 = input.parse::<u32>().unwrap();
     println!("Hello, world!");
-    let primesList: std::vec::Vec<Prime>;
-    let mut count: u32 = 1;
+    let mut primesList: Vec<Prime> = vec![];
+    let mut count: u32   = 1;
     while count < limit{
         let mut primeTrue:bool = true;
-        for p in primesList{
+        for p in &primesList{
             if count % p.1 == 0 {
                 primeTrue = false;
             }
         }
         if !primeTrue {
-            primesList.push((count, count))
+            let temp = Prime((count), (count));
+            primesList.push(temp)
         }
     }
+    for p in primesList{
+        print!("{}",p.0)
+    }
+    count = count + 1;
 
 }
