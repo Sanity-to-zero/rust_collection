4b798c5ce1a4b8a617164dde33b3fde2cf67e2c5
Pufferunpopped <blazingtorch.spam@gmail.com>
2022-11-08 23:10:55 -0800

    started structureing code

--- skiamaybe/skiatesthing/src/main.rs
+++ skiamaybe/skiatesthing/src/main.rs
@@ -1,3 +1,14 @@
+
+
+// universal constants
+let const g = 9.8
+
 fn main() {
     println!("Hello, world!");
+    struct equation{
+        str answer,
+        str[] requirements,
+        str equation
+    }
+    
 }
