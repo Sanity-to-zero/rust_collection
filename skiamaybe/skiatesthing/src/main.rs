f18436535ef3b3e097be61e6fe7a722cac0d6009
Pufferunpopped <blazingtorch.spam@gmail.com>
2022-11-14 22:49:58 -0800

    tried and tested fs temp removed skia dependency: no compiling meme now

--- skiamaybe/skiatesthing/src/main.rs
+++ skiamaybe/skiatesthing/src/main.rs
@@ -1,21 +1,24 @@
 
 
 // universal constants
-let const g = 9.8
+
 use std::io;
+use std::fs;
 fn main() {
     println!("Hello, world!");
+    
     let mut category = String::new();
     let mut inputs = String::new();
     println!("Please enter a category: 'kinematics' 'dynamics' 'energetics'");
     io::stdin().read_line(&mut category).expect("Could not read line");
     println!("Please enter what you wish to find as well as given numbers in m/s/kg form: 
-    \n eg:  'find a vi=0 t=10 d=20'");
+    \n eg:  'a vi=0 t=10 d=20'");
     io::stdin().read_line(&mut inputs).expect("Could not read line");
-    struct equation{
-        str answer,
-        str[] requirements,
-        str equation
-    }
+    let equations_file = fs::read_to_string("equations.txt");
+    // struct equation{
+    //     str answer,
+    //     str[] requirements,
+    //     str equation
+    // }
     
 }
