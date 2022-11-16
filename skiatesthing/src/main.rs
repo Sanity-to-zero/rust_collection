bda3ff7f85367aca8f51e6da64b1dc3277fb43a5
Pufferunpopped <blazingtorch.spam@gmail.com>
2022-11-15 23:36:07 -0800

    modified:   skiatesthing/src/main.rs fixed analyzer not finding file

--- skiatesthing/src/main.rs
+++ skiatesthing/src/main.rs
@@ -15,8 +15,15 @@ fn main() {
     println!("Please enter what you wish to find as well as given numbers in m/s/kg form: 
     \n eg:  'a vi=0 t=10 d=20'");
     io::stdin().read_line(&mut inputs).expect("Could not read line");
-    let equations_file = fs::read_to_string("equations.txt");
+    let equations_file = fs::read_to_string("equations.txt").unwrap();
+    let val_to_find = inputs.chars().next().unwrap();
     let mut possible: Vec<String> = vec![];
+    let mut eq_lines = equations_file.lines();
+    for line in eq_lines{
+        if line.starts_with(val_to_find) {
+            possible.push(String::from(line));
+        }
+    }
     // struct equation{
     //     str answer,
     //     str[] requirements,
