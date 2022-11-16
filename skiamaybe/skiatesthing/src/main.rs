8bd4d5636f83cae372d862f9faa78fe12177e371
Pufferunpopped <blazingtorch.spam@gmail.com>
2022-11-15 21:47:46 -0800

    styuff

--- skiamaybe/skiatesthing/src/main.rs
+++ skiamaybe/skiatesthing/src/main.rs
@@ -7,10 +7,11 @@ use std::fs;
 fn main() {
     println!("Hello, world!");
     
-    let mut category = String::new();
+    // let mut category = String::new();
+    // println!("Please enter a category: 'kinematics' 'dynamics' 'energetics'");
+    // io::stdin().read_line(&mut category).expect("Could not read line");
+    // not needed
     let mut inputs = String::new();
-    println!("Please enter a category: 'kinematics' 'dynamics' 'energetics'");
-    io::stdin().read_line(&mut category).expect("Could not read line");
     println!("Please enter what you wish to find as well as given numbers in m/s/kg form: 
     \n eg:  'a vi=0 t=10 d=20'");
     io::stdin().read_line(&mut inputs).expect("Could not read line");
