f309ee0d1e0e9b6c0aacfb51c4dac8a746fada0e
Pufferunpopped <blazingtorch.spam@gmail.com>
2022-12-05 23:06:17 -0800

    modified:   skiatesthing/src/sort_eq.py

--- skiatesthing/src/sort_eq.py
+++ skiatesthing/src/sort_eq.py
@@ -1,6 +1,15 @@
 
-with open("") as f:
+with open(".txt", "r") as f:
     lines = f.readlines()
+    collect = open(".txt", "w")
     for l in lines:
+        buff = ""
         #sort
-        l.split(";")
+        if l == "":
+            vars = l.split(";")[1]
+            vars = sorted(vars)
+            for s in vars:
+                buff += s
+        collect.write(buff)
+
+
