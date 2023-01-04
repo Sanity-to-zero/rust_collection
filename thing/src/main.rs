cb218acb4aaa83fe91a1c27bb3abb7a8c9b1b336
Sanity-to-zero <blazingtorch.spam@gmail.com>
2023-01-04 12:04:41 -0800

    modified:   thing/src/main.rs

--- thing/src/main.rs
+++ thing/src/main.rs
@@ -7,11 +7,27 @@
 //each loop check if any countdown is True
 //if all empty, add to list of primes and key/value
 
-struct Prime (u32, u32);
+
+use std::io;
+#[allow(unused)]
 fn main() {
-    /*let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("failed to readline");
-    let limit:u32 = input.parse::<u32>().unwrap();*/
+    let mut width: String = String::new();
+    let mut height: String = String::new();
+    let mut input = String::new();
+    io::stdin().read_line(&mut width).expect("failed to read line");
+    let limit:u32 = width.parse::<u32>().unwrap();
+    io::stdin().read_line(&mut height).expect("failed to read line");
+    let limit:u32 = height.parse::<u32>().unwrap();
+    io::stdin().read_line(&mut input).expect("failed to read line");
+    
     
 
+}
+#[allow(dead_code)]
+#[allow(unused)]
+fn get_data(size :u32)->(u32, u32){
+    for n in 1..size{
+        //
+    }
+    return (0,0);
 }
